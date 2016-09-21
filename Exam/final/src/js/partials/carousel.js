897d251db8951012014dbec296a461fa3d4ea4fb
Ievgen <john77777@ukr.net>
2016-09-21 08:34:20 +0000

    Exam

---
+++ Exam/final/src/js/partials/carousel.js
@@ -0,0 +1,29 @@
+'use strict';
+(function($) {
+    $(function() {
+        $('.jcarousel').jcarousel();
+        $('.jcarousel-control-prev')
+            .on('jcarouselcontrol:active', function() {
+                $(this).removeClass('inactive');
+            })
+            .on('jcarouselcontrol:inactive', function() {
+                $(this).addClass('inactive');
+            })
+            .jcarouselControl({
+                target: '-=1'
+            });
+
+        $('.jcarousel-control-next')
+            .on('jcarouselcontrol:active', function() {
+                $(this).removeClass('inactive');
+            })
+            .on('jcarouselcontrol:inactive', function() {
+                $(this).addClass('inactive');
+            })
+            .jcarouselControl({
+                target: '+=1'
+            });
+    });
+})(jQuery);
+Contact GitHub API Training Shop Blog About
+© 2016 GitHub, Inc. Terms 
